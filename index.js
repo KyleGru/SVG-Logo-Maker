c2796238a242f875d1941656401763d83fcf8723
KyleGru <136273960+KyleGru@users.noreply.github.com>
2024-01-02 16:15:49 -0500

    Made svg.js file, created tests in shapes.test.js, finished the function for inquirer to prompt the user

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
-const {Triangle, Square, Circle} = require('./lib/shapes');
+const { Circle, Triangle, Square } = require('./lib/shapes');
+const Svg = require("./lib/svg")
 
 const questions = [
     {
@@ -26,3 +27,33 @@ const questions = [
         message: 'Enter your shape color (CSS color keyword or haxadecimal number):',
     },
 ];
+
+inquirer
+    .prompt(questions)
+    .then((response) => {
+        let shape;
+        switch(response.shape) {
+            case 'triangle':
+                shape = new Triangle();
+                break;
+            case 'square':
+                shape = new Square();
+                break;
+            default:
+                shape = new Circle();
+        }
+
+        shape.setColor(response.shapeColor);
+        const svg = new Svg();
+        svg.setShape(shape);
+        svg.setText(response.text, response.textColor);
+
+        fs.writeFile(`examples/logo-${response.text}.svg`, svg.render(), (err) => {
+            if (err) console.log(err);
+            console.log('You have successfully created your logo!');
+        });
+    })
+
+    .catch((err) => {
+        console.log(err.message);
+    });
